030066a86e082a474f56c0d432a1f819d61fdc02
kossmar <kossmar2@gmail.com>
2018-02-16 17:42:33 -0800

    stuff

--- cmd/verbose.go
+++ cmd/verbose.go
@@ -11,7 +11,7 @@ var verboseCmd = &cobra.Command{
 	Short: "Show more price information",
 	Long:  `I don't know what else to tell you`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hugo Static Site Generator v0.9 -- HEAD")
+		fmt.Println("so verbose")
 	},
 }
 
